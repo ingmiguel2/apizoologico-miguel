fb97d9db17b72214fe5037acfb5f5a1c7e008135
mishasdk <micorreeoo>
2025-10-02 20:39:11 -0500

    se agregaron funciones del crud

--- src/routes/animal.js
+++ src/routes/animal.js
@@ -2,11 +2,27 @@ const express = require("express");
 const router = express.Router(); //manejador de rutas de express
 const animalSchema = require("../models/animalModel");
 //Nuevo animal
-router.post("/animals", (req, res) => {
+router.post("/animalitos", (req, res) => {
   const animal = animalSchema(req.body);
   animal
     .save()
     .then((data) => res.json(data))
     .catch((error) => res.json({ message: error }));
 });
+
+router.get("/animalitos",(req,res) => {
+animalSchema.find()
+     .then((data) => res.json(data))
+     .catch((error) => res.json({message: error}));
+});
+
+//Consultar un animal por su id
+router.get("/animalitos/:id", (req, res) => {
+ const { id } = req.params;
+  animalSchema
+ .findById(id)
+ .then((data) => res.json(data))
+ .catch((error) => res.json({ message: error }));
+});
+
 module.exports = router;
