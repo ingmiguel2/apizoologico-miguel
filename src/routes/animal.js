cb638829de8924cf76a0b8655dd9d9aee5f05f5c
ingmiguel2 <miguela.corream@konradlorenz.edu.co>
2025-10-19 23:04:54 -0500

    se agregaron mas metodos del crud http en animal.js en routes

--- src/routes/animal.js
+++ src/routes/animal.js
@@ -11,23 +11,10 @@ router.post("/animalitos", (req, res) => {
 });
 
 
-//1. Trae los datos de los animales con edad mayor a 2 de la base de datos
-router.get("/animalitos",(req,res) => {
-animalSchema.find({edad:{$gt:2}})
-     .then((data) => res.json(data))
-     .catch((error) => res.json({message: error}));
-});
 
-//2. Trae todos los  datos animales de la base de datos
+// Traer todos los  datos animales de la base de datos
 router.get("/animalitos/all",(req,res) => {
 animalSchema.find()
-     .then((data) => res.json(data))
-     .catch((error) => res.json({message: error}));
-});
-
-//3. Trae los animales de la bse de datos del tipo oveja
-router.get("/animalitos/ovejas",(req,res) => {
-animalSchema.find({tipo:{$eq:"Oveja"}})
      .then((data) => res.json(data))
      .catch((error) => res.json({message: error}));
 });
@@ -39,6 +26,49 @@ router.get("/animalitos/:id", (req, res) => {
  .findById(id)
  .then((data) => res.json(data))
  .catch((error) => res.json({ message: error }));
+});
+
+//Modificar el nombre de un animal por su id
+router.put("/animalitos/:id", (req, res) => {
+ const { id } = req.params;
+ const { nombre, edad, tipo, fecha } = req.body;
+ animalSchema
+ .updateOne({ _id: id }, {
+ $set: { nombre, edad, tipo, fecha }
+ })
+ .then((data) => res.json(data))
+ .catch((error) => res.json({ message: error }));
+});
+
+//Eliminar un animal por su id
+
+router.delete("/animalitos/:id", (req, res) => {
+ const { id } = req.params;
+ animalSchema
+ .findByIdAndDelete(id)
+ .then((data) => {
+ res.json(data);
+ })
+ .catch((error) => {
+ res.json({ message: error });
+ });
+});
+
+
+
+//2. Trae los datos de los animales con edad mayor a 2 de la base de datos
+router.get("/animalitos",(req,res) => {
+animalSchema.find({edad:{$gt:2}})
+     .then((data) => res.json(data))
+     .catch((error) => res.json({message: error}));
+});
+
+
+//3. Trae los animales de la bse de datos del tipo oveja
+router.get("/animalitos/ovejas",(req,res) => {
+animalSchema.find({tipo:{$eq:"Oveja"}})
+     .then((data) => res.json(data))
+     .catch((error) => res.json({message: error}));
 });
 
 module.exports = router;
